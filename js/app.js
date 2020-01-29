7273186d3bd46bb4a3d71a0a33a6c94731c1bd49
Dillon Duguid <31974710+dillonduguid@users.noreply.github.com>
2020-01-29 11:14:16 +0000

    Initial commit
    Interactive map works, marker follows mouse.
    https://developer.here.com/tutorials/javascript-api/

---
+++ js/app.js
@@ -0,0 +1,53 @@
+import { $, $$, to24HourFormat, formatRangeLabel, toDateInputFormat } from './helpers.js';
+import { center, hereCredentials } from './config.js';
+
+//Height calculations
+const height = $('#content-group-1').clientHeight || $('#content-group-1').offsetHeight;
+$('.content').style.height = height + 'px';
+
+// Initialize HERE Map
+const platform = new H.service.Platform({ apikey: hereCredentials.apikey });
+const defaultLayers = platform.createDefaultLayers();
+const map = new H.Map(document.getElementById('map'),       defaultLayers.vector.normal.map, {
+   center,
+   zoom: 12,
+   pixelRatio: window.devicePixelRatio || 1
+});
+const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+const provider = map.getBaseLayer().getProvider();
+
+//Initialize router and geocoder
+const router = platform.getRoutingService();
+const geocoder = platform.getGeocodingService();
+
+window.addEventListener('resize', () => map.getViewPort().resize());
+
+export { router, geocoder }
+
+
+/* ...
+ * map and platform code from previous section goes here
+ * ...
+ */
+
+let polygon;
+const marker = new H.map.Marker(center, {volatility: true});
+marker.draggable = true;
+map.addObject(marker);
+
+// Add event listeners for marker movement
+map.addEventListener('dragstart', evt => {
+   if (evt.target instanceof H.map.Marker) behavior.disable();
+}, false);
+map.addEventListener('dragend', evt => {
+   if (evt.target instanceof H.map.Marker) {
+      behavior.enable();
+      calculateIsoline();
+   }
+}, false);
+map.addEventListener('drag', evt => {
+   const pointer = evt.currentPointer;
+   if (evt.target instanceof H.map.Marker) {
+     evt.target.setGeometry(map.screenToGeo(pointer.viewportX, pointer.viewportY));
+   }
+}, false);
---
+++ js/config.js
@@ -0,0 +1,12 @@
+const center = {
+   lat: 52.5159,
+   lng: 13.3777,
+   text: 'Berlin, Germany'
+}
+
+const hereCredentials = {
+   id: ' 2M7KdftQr2bVN7WxM3AM ',
+   apikey: 'iYvMpF1KOQJ89K6boSx23q7-l9Xtv4Jz3a5fun9MS1w'
+}
+
+export { center, hereCredentials };
