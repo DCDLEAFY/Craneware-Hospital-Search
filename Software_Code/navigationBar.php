0801956b64f6878b851bb71d83233e3947d44f52
Costin Grigore <43765185+costingrigore@users.noreply.github.com>
2020-02-03 18:18:41 +0000

    Changes in hospitalBasedOnProcedureID
    I made chages on how the tables show in hospitalBasedOnProcedureID

--- Software_Code/navigationBar.php
+++ Software_Code/navigationBar.php
@@ -4,7 +4,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
       <!-- Title displayed in the navigation bar, showing the name of the company -->
-      <a class="navbar-brand" href="#">Craneware</a>
+      <a class="navbar-brand" href="index.php">Craneware</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
