3b5d229b705ebc1e0061cf3588cc4a53f346498e
josepht1108 <josepht1108@gmail.com>
2020-02-04 15:34:51 +0000

    a littel change

--- JSUnitTesting/test/average.js
+++ JSUnitTesting/test/average.js
@@ -1,15 +1,15 @@
-const should = require('should')
-const average = require('../src/average')
+const should = require('should');
+const average = require('../src/average');
 
 describe('#average',()=>{
     it('shoule return the average of array',done =>{
-        var avg = average([1,2,3,4])
+        var avg = average([1,2,3,4]);
         avg.should.equal(2.5)
         done()
     })
 
     it('should return NaN when array is empty', done => {
-        var avg = average([])
+        var avg = average([]);
         isNaN(avg).should.be.true
         done()
       })
--- JSUnitTesting/test/max.js
+++ JSUnitTesting/test/max.js
@@ -1,17 +1,17 @@
-const should = require('should')
-const max = require('../src/max')
+const should = require('should');
+const max = require('../src/max');
 
 describe('#max', () => {
     // 測試有沒有取得正確的最大值
     it('should return the maximum in array', done => {
-      var maximum = max([1, 10, 100, 1000])
+      var maximum = max([1, 10, 100, 1000]);
       maximum.should.equal(1000)
       done()
     })
     // 測試有沒有回傳 undefined
     it('should return undefined when array is empty', done => {
-      const maximum = max([])
-      ;(maximum === undefined).should.be.true
+      const maximum = max([]);
+      (maximum === undefined).should.be.true
       done()
     })
   })
