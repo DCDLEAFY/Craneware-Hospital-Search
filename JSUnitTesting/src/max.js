a045db2f2759d611345515350478665a3b9d09e5
Josepht1108 <56254046+Josepht1108@users.noreply.github.com>
2020-02-04 12:24:35 +0000

    Add files via upload

---
+++ JSUnitTesting/src/max.js
@@ -0,0 +1,8 @@
+module.exports = function max(array) {
+    let max = array[0]
+    for (let i = 0; i < array.length; i++) {
+      max = array[i] > max ? array[i] : max
+    }
+    return max
+  }
+  
---
+++ JSUnitTesting/src/min.js
@@ -0,0 +1,8 @@
+module.exports = function min(array) {
+    let min = array[0]
+    for (let i = 0; i < array.length; i++) {
+      min = array[i] < min ? array[i] : min
+    }
+    return min
+  }
+  
